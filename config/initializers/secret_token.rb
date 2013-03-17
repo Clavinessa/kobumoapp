c51b12a076a95e553226dfcbf3329035738b4fc0
Diana Brusnikina <clavinessa@ubuntu.(none)>
2013-03-16 23:02:12 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kobumo::Application.config.secret_token = 'fa8037f82a4cfbe628ff6100bc9fc45c87c7b5782bd04b7f383aeb2772bbec2d6650144218d0f15529f7dcb1a7e4abd5bbe405000b7946c25ea8092af157be55'
